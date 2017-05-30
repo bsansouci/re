6c229c9ae38a2e875f925dcc81021a91426caa82
Benjamin San Souci <benjamin.sansouci@gmail.com>
2017-05-29 22:45:20 -0700

    manual module aliasing.

---
+++ lib/ocamlRe.ml
@@ -0,0 +1,17 @@
+module Re = Re
+
+module Re_pcre = Re_pcre
+
+module Re_glob = Re_glob
+
+module Re_perl = Re_perl
+
+module Re_posix = Re_posix
+
+module Re_str = Re_str
+
+module Re_emacs = Re_emacs
+
+module Re_cset = Re_cset
+
+module Re_automata = Re_automata
